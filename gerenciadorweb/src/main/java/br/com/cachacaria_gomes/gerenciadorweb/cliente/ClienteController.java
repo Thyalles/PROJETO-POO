b000e24cf179870ac9604f84f1ddc905fe195820
Thyalles Campos <81724859+Thyalles@users.noreply.github.com>
2025-11-07 02:16:32 -0300

    Update ClienteController.java

--- gerenciadorweb/src/main/java/br/com/cachacaria_gomes/gerenciadorweb/cliente/ClienteController.java
+++ gerenciadorweb/src/main/java/br/com/cachacaria_gomes/gerenciadorweb/cliente/ClienteController.java
@@ -1,11 +1,11 @@
 package br.com.cachacaria_gomes.gerenciadorweb.cliente;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -56,4 +56,5 @@ public class ClienteController {
     }
 
 }
+
 
