fbc837467e0f64886064abb4a83f1f43f6248c65
Thyalles Campos <81724859+Thyalles@users.noreply.github.com>
2025-11-07 02:07:44 -0300

    Add CORS support to ClienteController

--- gerenciadorweb/src/main/java/br/com/cachacaria_gomes/gerenciadorweb/cliente/ClienteController.java
+++ gerenciadorweb/src/main/java/br/com/cachacaria_gomes/gerenciadorweb/cliente/ClienteController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/clientes")
 public class ClienteController {
 
@@ -52,4 +53,5 @@ public class ClienteController {
         
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors.toString());
     }
+
 }
