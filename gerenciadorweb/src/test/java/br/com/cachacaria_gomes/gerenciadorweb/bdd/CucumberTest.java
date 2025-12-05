09e9cbbca03d6469414eb157ced6429ecf0d32b9
Thyalles Campos <81724859+Thyalles@users.noreply.github.com>
2025-12-05 15:54:12 -0300

    Add CucumberTest class for BDD testing

---
+++ gerenciadorweb/src/test/java/br/com/cachacaria_gomes/gerenciadorweb/bdd/CucumberTest.java
@@ -0,0 +1,16 @@
+package br.com.cachacaria_gomes.bdd;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("favoritos.feature")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class CucumberTest {
+
+}
